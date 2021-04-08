4daa7f353b96ef0d47e5edebe7e6cbdc58ecb75f
Planoetscher Daniel (Student Com20) <daniel.planoetscher@stud-inf.unibz.it>
2021-04-08 22:47:00 +0200

    basic directory structure

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import App from 'components/App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
