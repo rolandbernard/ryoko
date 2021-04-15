134316934c0afd6cda19e292843eae8cefdab137
Planoetscher Daniel (Student Com20) <daniel.planoetscher@stud-inf.unibz.it>
2021-04-15 09:32:29 +0200

    new pages and components for later / fixed routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,24 @@
 
 import { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { isLoggedIn } from 'adapters/api';
 
 const Home = lazy(() => import('pages/Home'));
 const Login = lazy(() => import('pages/Login'));
+const Register = lazy(() => import('pages/Register'));
+const Tasks = lazy(() => import('pages/Tasks'));
+const Navigation = lazy(() => import('components/ui/Navigation'));
 
 export default function App() {
     return (
         <Router>
             <Suspense fallback={false}>
+                {isLoggedIn() && <Navigation />}
                 <Switch>
-                    <Route path="/login" component={Login} />
-                    <Route path="/" component={Home} />
+                    {!isLoggedIn() && <Route path="/register" component={Register} />}
+                    {!isLoggedIn() && <Route exact path="/" component={Home} />}
+                    {!isLoggedIn() && <Route path="/" component={Login} />}
+                    <Route path="/" component={Tasks} />
                 </Switch>
             </Suspense>
         </Router>
---
+++ client/src/components/ui/Navigation/index.tsx
@@ -0,0 +1,9 @@
+import './navigation.scss';
+
+export default function Navigation() {
+    return (
+        <main>
+            <h1>Navigation</h1>
+        </main>
+    );
+}
