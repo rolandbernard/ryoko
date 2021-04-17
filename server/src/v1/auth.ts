8c86ad6cfc75f77e6a8b6f7714ffcb7091c5c208
Roland Bernard <rolbernard@unibz.it>
2021-04-17 14:17:30 +0200

    Added options to update user data

--- server/src/v1/auth.ts
+++ server/src/v1/auth.ts
@@ -10,104 +10,9 @@ import { getPublicKey, getPrivateKey } from '../keys';
 
 const auth = express();
 
-interface RegisterBody {
-    username: string;
-    password: string;
+export interface Token {
+    id: string;
 }
-
-auth.post('/register', async (req, res) => {
-    if (isOfType<RegisterBody>(req.body, [['username', 'string'], ['password', 'string']])) {
-        const body: RegisterBody = req.body;
-        const id = uuid();
-        const passwdHash = await hash(body.password, 10);
-        try {
-            await database('users').insert({
-                id: id,
-                user_name: body.username,
-                passwd_hash: passwdHash
-            });
-            res.status(200).json({
-                status: 'success',
-            });
-        } catch (e) {
-            // Fails if unique constraint for username is not met
-            res.status(400).json({
-                status: 'error',
-                message: 'failed to create user',
-            });
-        }
-    } else {
-        res.status(400).json({
-            status: 'error',
-            message: 'missing request fields',
-        });
-    }
-});
-
-interface TokenBody {
-    username: string;
-    password: string;
-}
-
-auth.post('/token', async (req, res) => {
-    if (isOfType<TokenBody>(req.body, [['username', 'string'], ['password', 'string']])) {
-        const body: TokenBody = req.body;
-        try {
-            const user = await database('users').where({ user_name: body.username });
-            if (user.length === 1) {
-                if (await compare(body.password, user[0].passwd_hash)) {
-                    const token = await asyncify(sign, {
-                        id: user[0].id,
-                    }, await getPrivateKey(), { algorithm: "ES384", expiresIn: 60 * 60 });
-                    res.status(200).json({
-                        status: 'success',
-                        token: token,
-                    });
-                } else {
-                    res.status(400).json({
-                        status: 'error',
-                        message: 'wrong username or password',
-                    });
-                }
-            } else {
-                res.status(400).json({
-                    status: 'error',
-                    message: 'username not found',
-                });
-            }
-        } catch (e) {
-            console.error(e);
-            res.status(400).json({
-                status: 'error',
-                message: 'failed to acquire token',
-            });
-        }
-    } else {
-        res.status(400).json({
-            status: 'error',
-            message: 'missing request fields',
-        });
-    }
-});
-
-auth.use(requireVerification);
-
-auth.get("/extend", async function (req, res) {
-    if (req.body?.token) {
-        const token = await asyncify(sign, {
-            id: req.body.token.id,
-        }, await getPrivateKey(), { algorithm: "ES384", expiresIn: 60 * 60 });
-        res.status(200).json({
-            status: 'success',
-            token: token,
-        });
-    } else {
-        res.status(403).json({
-            status: 'error',
-            message: 'authentication failed',
-        });
-    }
-});
 
 export async function tokenVerification(req: Request, _res: Response, next: NextFunction) {
     const header = req.headers?.authorization;
@@ -143,6 +48,176 @@ export function requireVerification(req: Request, res: Response, next: NextFunct
         });
     }
 }
+
+async function generateToken(token: Token) {
+    return asyncify(sign, token, await getPrivateKey(), { algorithm: "ES384", expiresIn: 60 * 60 });
+}
+
+interface RegisterBody {
+    username: string;
+    password: string;
+    email?: string;
+    realname?: string;
+}
+
+auth.post('/register', async (req, res) => {
+    if (isOfType<RegisterBody>(req.body, [['username', 'string'], ['password', 'string']])) {
+        const body: RegisterBody = req.body;
+        const id = uuid();
+        const passwdHash = await hash(body.password, 10);
+        try {
+            const token = await generateToken({ id: id });
+            await database('users').insert({
+                id: id,
+                user_name: body.username,
+                passwd_hash: passwdHash,
+                email: body.email ?? null,
+                real_name: body.realname ?? null,
+            });
+            res.status(200).json({
+                status: 'success',
+                token: token,
+            });
+        } catch (e) {
+            // Fails if unique constraint for username is not met
+            res.status(400).json({
+                status: 'error',
+                message: 'failed to create user',
+            });
+        }
+    } else {
+        res.status(400).json({
+            status: 'error',
+            message: 'missing request fields',
+        });
+    }
+});
+
+interface TokenBody {
+    username: string;
+    password: string;
+}
+
+auth.post('/token', async (req, res) => {
+    if (isOfType<TokenBody>(req.body, [['username', 'string'], ['password', 'string']])) {
+        const body: TokenBody = req.body;
+        try {
+            const user = await database('users').where({ user_name: body.username });
+            if (user.length === 1) {
+                if (await compare(body.password, user[0].passwd_hash)) {
+                    const token = await generateToken({ id: user[0].id });
+                    res.status(200).json({
+                        status: 'success',
+                        token: token,
+                    });
+                } else {
+                    res.status(400).json({
+                        status: 'error',
+                        message: 'wrong username or password',
+                    });
+                }
+            } else {
+                res.status(400).json({
+                    status: 'error',
+                    message: 'username not found',
+                });
+            }
+        } catch (e) {
+            res.status(400).json({
+                status: 'error',
+                message: 'failed to acquire token',
+            });
+        }
+    } else {
+        res.status(400).json({
+            status: 'error',
+            message: 'missing request fields',
+        });
+    }
+});
+
+auth.use(requireVerification);
+
+auth.get("/extend", async function (req, res) {
+    if (req.body?.token) {
+        const token = await asyncify(sign, {
+            id: req.body.token.id,
+        }, await getPrivateKey(), { algorithm: "ES384", expiresIn: 60 * 60 });
+        res.status(200).json({
+            status: 'success',
+            token: token,
+        });
+    } else {
+        res.status(403).json({
+            status: 'error',
+            message: 'authentication failed',
+        });
+    }
+});
+
+interface UsernameBody {
+    token: Token;
+    username: string;
+}
+
+auth.put("/username", async function (req, res) {
+    if (isOfType<UsernameBody>(req.body, [['username', 'string']])) {
+        const body: UsernameBody = req.body;
+        try {
+            await database('users').update({
+                user_name: body.username,
+            }).where({
+                id: body.token.id,
+            });
+            res.status(200).json({
+                status: 'success',
+            });
+        } catch (e) {
+            // Fails if unique constraint for username is not met
+            res.status(400).json({
+                status: 'error',
+                message: 'failed to update username',
+            });
+        }
+    } else {
+        res.status(400).json({
+            status: 'error',
+            message: 'missing request fields',
+        });
+    }
+});
+
+interface PasswordBody {
+    token: Token;
+    password: string;
+}
+
+auth.put("/password", async function (req, res) {
+    if (isOfType<PasswordBody>(req.body, [['password', 'string']])) {
+        const body: PasswordBody = req.body;
+        try {
+            const passwdHash = await hash(body.password, 10);
+            await database('users').update({
+                passwd_hash: passwdHash
+            }).where({
+                id: body.token.id,
+            });
+            res.status(200).json({
+                status: 'success',
+            });
+        } catch (e) {
+            res.status(400).json({
+                status: 'error',
+                message: 'failed to update password',
+            });
+        }
+    } else {
+        res.status(400).json({
+            status: 'error',
+            message: 'missing request fields',
+        });
+    }
+});
 
 export default auth;
 
--- server/src/v1/user.ts
+++ server/src/v1/user.ts
@@ -1,8 +1,10 @@
 
 import express from 'express';
+import { validate } from 'uuid';
 
 import database from '../database';
-import { requireVerification } from './auth';
+import { isOfType } from '../util';
+import { requireVerification, Token } from './auth';
 
 const user = express();
 
@@ -12,8 +14,6 @@ user.get('/name/:username', async (req, res) => {
             .select({
                 id: 'id',
                 username: 'user_name',
-                email: 'email',
-                realname: 'real_name',
             })
             .where({ username: req.params.username });
         if (user.length === 1) {
@@ -56,6 +56,85 @@ user.get('/', async (req, res) => {
             res.status(404).json({
                 status: 'error',
                 message: 'user not found',
+            });
+        }
+    } catch (e) {
+        res.status(400).json({
+            status: 'error',
+            message: 'failed get user',
+        });
+    }
+});
+
+interface UserUpdateBody {
+    token: Token;
+    realname?: string;
+    email?: string;
+}
+
+user.put('/', async (req, res) => {
+    if (
+        isOfType<UserUpdateBody>(req.body, [['realname', 'string']])
+        || isOfType<UserUpdateBody>(req.body, [['email', 'string']])
+    ) {
+        try {
+            const updated = await database('users')
+                .update({
+                    email: req.body.email,
+                    real_name: req.body.realname,
+                })
+                .where({ id: req.body.token.id });
+            if (updated === 1) {
+                res.status(200).json({
+                    status: 'success',
+                });
+            } else {
+                res.status(404).json({
+                    status: 'error',
+                    message: 'user not found',
+                });
+            }
+        } catch (e) {
+            res.status(400).json({
+                status: 'error',
+                message: 'failed to update user',
+            });
+        }
+    } else {
+        res.status(400).json({
+            status: 'error',
+            message: 'missing request fields',
+        });
+    }
+});
+
+user.get('/:uuid', async (req, res) => {
+    try {
+        const id = req.params.uuid;
+        if (validate(id)) {
+            const user = await database('users')
+            .select({
+                id: 'id',
+                username: 'user_name',
+                email: 'email',
+                realname: 'real_name',
+            })
+            .where({ id: id });
+            if (user.length === 1) {
+                res.status(200).json({
+                    status: 'success',
+                    user: user[0],
+                });
+            } else {
+                res.status(404).json({
+                    status: 'error',
+                    message: 'user not found',
+                });
+            }
+        } else {
+            res.status(400).json({
+                status: 'error',
+                message: 'malformed uuid',
             });
         }
     } catch (e) {
