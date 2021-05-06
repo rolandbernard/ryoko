a5e9b5dac18ba250f62f9c828b771aa90382ea75
Planoetscher Daniel (Student Com20) <daniel.planoetscher@stud-inf.unibz.it>
2021-05-06 23:15:31 +0200

    dropdown

---
+++ client/src/components/navigation/Dropdown/index.tsx
@@ -0,0 +1,31 @@
+import { ReactNode, useState } from 'react';
+import { Link } from 'react-router-dom';
+import './dropdown.scss';
+
+interface DropDownItem {
+    label: string;
+    route: string;
+}
+
+interface Props {
+    children: ReactNode;
+    items: DropDownItem[]
+}
+
+export default function Dropdown({ children, items }: Props) {
+    const [isOpen, setOpen] = useState(false);
+    return (
+        <div className={'dropdown-container' + (isOpen ? ' open' : '')} onClick={() => setOpen(state => !state)}>
+            {children}
+            <div className="dropdown">
+                {
+                    items.map((item) => (
+                        <Link className="dropdown-item" to={item.route}>
+                            {item.label}
+                        </Link>
+                    ))
+                }
+            </div>
+        </div>
+    );
+}
--- client/src/pages/Teams/TeamsStats/index.tsx
+++ client/src/pages/Teams/TeamsStats/index.tsx
@@ -1,9 +1,18 @@
+import Dropdown from 'components/navigation/Dropdown';
 import './teams-stats.scss';
 
 export default function TeamsStats() {
     return (
         <section className="teams-stats-section">
             <div className="content-container">
+                <Dropdown items={[{ label: "Last month", route: "lastmonth" }]}>
+                    <div className="filter-label">
+                        <span className="material-icons icon">
+                            expand_more
+                        </span>
+                        Last week
+                        </div>
+                </Dropdown>
                 test
             </div>
         </section>
