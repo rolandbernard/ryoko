69b9c7e0677191125301fb72372b3ea12708f278
Roland Bernard <rolbernard@unibz.it>
2021-05-09 14:22:42 +0200

    Added adapters for comments

--- client/src/adapters/comment.ts
+++ client/src/adapters/comment.ts
@@ -1,4 +1,6 @@
 
+import { executeApiGet, executeApiPost, executeApiPut } from './util';
+
 export interface Comment {
     id: string;
     task: string;
@@ -6,5 +8,26 @@ export interface Comment {
     text: string;
     created: Date;
     edited: Date;
+}
+
+export function getComment(uuid: string): Promise<Comment> {
+    return executeApiGet(`comment/${uuid}`, ({ comment }) => ({
+        ...comment,
+        edited: new Date(comment.edited),
+        created: new Date(comment.created),
+    }), "Failed to get comment");
+}
+
+interface AddCommentBody {
+    task: string;
+    text: string;
+}
+
+export function createComment(comment: AddCommentBody): Promise<string> {
+    return executeApiPost(`comment`, comment, ({ id }) => id, "Failed to create comment");
+}
+
+export function updateComment(uuid: string, text: string) {
+    return executeApiPut(`comment/${uuid}`, { text: text }, () => {}, "Failed to update comment");
 }
 
