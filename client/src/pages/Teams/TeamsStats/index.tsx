eaac91b8e502c0de77464c29383ff71408082eac
Planoetscher Daniel (Student Com20) <daniel.planoetscher@stud-inf.unibz.it>
2021-05-21 09:12:25 +0200

    added user stats page

--- client/src/pages/Teams/TeamsStats/index.tsx
+++ client/src/pages/Teams/TeamsStats/index.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react';
 import { getTeamActivity, getTeamCompletion } from 'adapters/team';
 import LoadingScreen from 'components/ui/LoadingScreen';
 import { CompletionProps } from 'components/ui/Completion';
-import { StatusColors } from 'adapters/task';
+import { parseActivity, parseCompletion } from 'adapters/util';
 
 interface Props {
     teamId: string;
@@ -18,37 +18,8 @@ export default function TeamsStats({ teamId }: Props) {
 
 
     useEffect(() => {
-        getTeamActivity(teamId).then((a) => {
-            setActivity(a.map(item => ({
-                label: item.day,
-                value: item.time
-            })));
-        });
-        getTeamCompletion(teamId).then((comp) => {
-            const allAmount = comp.sum ?? 1;
-            setCompletions([
-                {
-                    label: 'Closed',
-                    percent: comp.closed / allAmount * 100,
-                    color: StatusColors.get('closed') ?? ''
-                },
-                {
-                    label: 'Open',
-                    percent: comp.open / allAmount * 100,
-                    color: StatusColors.get('open') ?? ''
-                },
-                {
-                    label: 'Suspended',
-                    percent: comp.suspended / allAmount * 100,
-                    color: StatusColors.get('suspended') ?? ''
-                },
-                {
-                    label: 'Overdue',
-                    percent: comp.overdue / allAmount * 100,
-                    color: StatusColors.get('overdue') ?? ''
-                },
-            ]);
-        })
+        getTeamActivity(teamId).then((a) => setActivity(parseActivity(a)));
+        getTeamCompletion(teamId).then((comp) => setCompletions(parseCompletion(comp)))
     }, [teamId])
     return (
         <section className="teams-stats-section">
