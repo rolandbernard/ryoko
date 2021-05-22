465a0004bc12a549bd8f672133fc2a468e0d7844
Roland Bernard <rolbernard@unibz.it>
2021-05-22 14:35:35 +0200

    More work on time formating

--- client/src/timely.ts
+++ client/src/timely.ts
@@ -1,29 +1,69 @@
 
 const UNITS = {
-    'millisecond':  1,
-    'second':       1000,
-    'minute':       1000 * 60,
-    'hour':         1000 * 60 * 60,
-    'day':          1000 * 60 * 60 * 24,
-    'week':         1000 * 60 * 60 * 24 * 7,
+    'year':         1000 * 60 * 60 * 24 * 365.2425,
     'month':        1000 * 60 * 60 * 24 * 30,
-    'year':         1000 * 60 * 60 * 24 * 365.25,
+    'week':         1000 * 60 * 60 * 24 * 7,
+    'day':          1000 * 60 * 60 * 24,
+    'hour':         1000 * 60 * 60,
+    'minute':       1000 * 60,
+    'second':       1000,
+    'millisecond':  1,
 };
 
 type Unit = (keyof typeof UNITS);
 
-export function formatDuration(millis: number, precision: Unit = 'minute'): string {
-    if (millis < UNITS[precision]) {
-        return 'instant';
+function formatAmount(amount: number, base: string): string {
+    amount = Math.round(amount);
+    if (amount === 0) {
+        return 'zero ' + base + 's';
+    } else if (amount === 1) {
+        return 'one ' + base;
     } else {
-        // TODO
-        return '';
+        return amount.toString() + ' ' + base + 's';
     }
 }
 
-export function formatTime(): string {
-    // TODO
-    return '';
+export function formatDuration(millis: number, precision: Unit = 'minute'): string {
+    if (millis >= UNITS[precision]) {
+        for (const key of (Object.keys(UNITS) as Unit[])) {
+            if (millis >= UNITS[key]) {
+                return formatAmount(millis / UNITS[key], key);
+            }
+        }
+    }
+    return 'moments';
+}
+
+function formatOrdinal(value: number): string {
+    value = Math.round(value);
+    if (value === 1 || (value % 10 === 1 && value > 20)) {
+        return value.toString() + 'st';
+    } else if (value === 2 || (value % 10 === 2 && value > 20)) {
+        return value.toString() + 'nd';
+    } else if (value === 3 || (value % 10 === 3 && value > 20)) {
+        return value.toString() + 'rd';
+    } else {
+        return value.toString() + 'th';
+    }
+}
+
+const MONTHS = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+];
+
+export function formatTime(date: Date): string {
+    return formatOrdinal(date.getDate()) + ' ' + MONTHS[date.getMonth()] + ' ' + date.getFullYear().toString();
 }
 
 export function formatRelativeTime(): string {
@@ -32,7 +72,22 @@ export function formatRelativeTime(): string {
 }
 
 export function addTime(date: Date, time: number, unit: Unit): Date {
-    return new Date(date.getTime() + time * UNITS[unit]);
+    if (time === 0) {
+        return new Date(date);
+    } else if (unit === 'month') {
+        const full = Math.floor(time);
+        const result = new Date(date);
+        result.setMonth(result.getMonth() + full);
+        return addTime(result, 30 * (time - full), 'day');
+    } else if (unit === 'year') {
+        const full = Math.floor(time);
+        const result = new Date(date);
+        result.setFullYear(result.getFullYear() + full);
+        return addTime(result, 12 * (time - full), 'month');
+    } else {
+        // All other durations are constant
+        return new Date(date.getTime() + time * UNITS[unit]);
+    }
 }
 
 export function subtractTime(date: Date, time: number, unit: Unit): Date {
