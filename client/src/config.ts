4a49ae423a07d8c1cb48cc0dc26728dbead2cb78
Roland Bernard <rolbernard@unibz.it>
2021-05-24 23:40:03 +0200

    index.html is now served as default for non-/api requests

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
 export let apiRoot: string;
 
 if (process.env.NODE_ENV === 'production') {
-    apiRoot = `${window.location.origin}/v1`;
+    apiRoot = `${window.location.origin}/api/v1`;
 } else {
-    apiRoot = `http://localhost:8000/v1`;
+    apiRoot = `http://localhost:8000/api/v1`;
 }
 
