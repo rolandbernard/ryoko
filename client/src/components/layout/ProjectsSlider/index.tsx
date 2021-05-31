afd543f0c158ccd62143f6ec61b035cde6feddc4
Roland Bernard <rolbernard@unibz.it>
2021-05-31 18:02:54 +0200

    Replaced the project slider

--- client/src/components/layout/ProjectsSlider/index.tsx
+++ client/src/components/layout/ProjectsSlider/index.tsx
@@ -1,42 +1,46 @@
 
-import Carousel from 'react-alice-carousel';
+import { useCallback, useRef } from 'react';
 
 import ProjectSlide, { ProjectSlideProps } from 'components/ui/ProjectSlide';
 
 import './projects-slider.scss';
-import 'react-alice-carousel/lib/alice-carousel.css';
-
-const responsive = {
-    0: { items: 1 },
-    560: { items: 2 },
-    1200: { items: 3 },
-};
 
 interface Props {
     projects: ProjectSlideProps[]
 }
 
 export default function ProjectsSlider({ projects }: Props) {
+    const slider = useRef<HTMLDivElement | null>(null);
+
+    const onChange = useCallback(amount => {
+        const current = parseFloat(slider.current?.style.getPropertyValue('--position') ?? '0') || 0;
+        const next = Math.min(Math.max(current + amount, 0), projects.length - 1);
+        slider.current?.style.setProperty('--position', next.toString());
+    }, [slider, projects]);
+
     return (
-        <>
-            {
-                projects.length ?
-                    <div className="project-slider">
-                        <Carousel
-                            mouseTracking
-                            responsive={responsive}
-                            disableDotsControls
-                            disableButtonsControls
-                            touchTracking
-                        >
-                            {
-                                projects.map(project => <ProjectSlide key={project.project.id} {...project} />)
-                            }
-                        </Carousel>
-                    </div> :
-                    <div>No Projects found.</div>
-            }
-        </>
+        projects.length
+            ? (
+                <div className="project-slider" ref={slider}>
+                    <div className="prev-button" onClick={() => onChange(-1)}>
+                        &lt;
+                    </div>
+                    <div className = "next-button" onClick={() => onChange(1)}>
+                        &gt;
+                    </div>
+                    {
+                        projects.map(project =>
+                            <div
+                                key={project.project.id}
+                                className="slide"
+                            >
+                                <ProjectSlide {...project} />
+                            </div>
+                        )
+                    }
+                </div>
+            )
+            : <div>No Projects found.</div>
     );
 }
 
