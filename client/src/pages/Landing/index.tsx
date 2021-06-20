aab670ea3d61906f56e0adcd9b3c8bd4474b6e79
Planoetscher Daniel (Student Com20) <daniel.planoetscher@stud-inf.unibz.it>
2021-06-20 15:07:48 +0200

    added some components to new landing page

--- client/src/pages/Landing/index.tsx
+++ client/src/pages/Landing/index.tsx
@@ -3,11 +3,15 @@ import './landing.scss';
 import React from 'react';
 import Page from 'components/layout/Page';
 import Intro from './partials/Intro';
+import Features from './partials/Features';
+import AboutApp from './partials/AboutApp';
 
 export default function LandingPage(): JSX.Element {
     return (
         <Page className="landing-page">
             <Intro />
+            <Features />
+            <AboutApp />
         </Page>
     )
 }
