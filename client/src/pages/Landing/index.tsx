6803e5abb73222a854e4a5dd9afcc86190deaf8b
Planoetscher Daniel (Student Com20) <daniel.planoetscher@stud-inf.unibz.it>
2021-06-20 17:51:21 +0200

    added all components

--- client/src/pages/Landing/index.tsx
+++ client/src/pages/Landing/index.tsx
@@ -7,6 +7,7 @@ import Features from './partials/Features';
 import AboutApp from './partials/AboutApp';
 import Team from './partials/Team';
 import Contact from './partials/Contact';
+import Logo from 'images/logo.svg';
 
 export default function LandingPage(): JSX.Element {
     return (
@@ -16,6 +17,19 @@ export default function LandingPage(): JSX.Element {
             <AboutApp />
             <Team />
             <Contact />
+            <footer>
+                <div className="content-container footer-container">
+                    <div className="footer-copyright">
+                        <img src={Logo} className="logo" alt="" width="70" height="24" />
+                        <p>
+                            &copy; <a href="index.html">ryoko</a>, 2021
+                        </p>
+                        <p>
+                            All rights reserved.
+                        </p>
+                    </div>
+                </div>
+            </footer>
         </Page>
     )
 }
