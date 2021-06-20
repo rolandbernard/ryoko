3d1cba9c52a8023a5d1744f1beaf5a0da01ae868
Planoetscher Daniel (Student Com20) <daniel.planoetscher@stud-inf.unibz.it>
2021-06-20 21:57:41 +0200

    open popups in portals

--- client/src/components/ui/Popup/index.tsx
+++ client/src/components/ui/Popup/index.tsx
@@ -1,7 +1,10 @@
 
 import { ReactNode } from 'react';
+import { createPortal } from 'react-dom';
 
 import './popup.scss';
+
+const root = document.getElementById('root') ?? document.getElementsByTagName('html')[0];
 
 interface Props {
     children: ReactNode
@@ -10,17 +13,18 @@ interface Props {
 
 export default function Popup({ children, onClose }: Props) {
     document.addEventListener('keydown', (e) => {
-        if(e.key === "Escape") {
+        if (e.key === "Escape") {
             onClose();
         }
     });
-    return (
+    return createPortal(
+
         <div className="popup-container">
             <div className="popup">
                 {children}
             </div>
             <div className="background" onClick={() => onClose()}></div>
-        </div>
+        </div>, root
     )
 }
 
