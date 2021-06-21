d951ca8a93ed998b168726949c54ee425d766592
Planoetscher Daniel (Student Com20) <daniel.planoetscher@stud-inf.unibz.it>
2021-06-21 11:34:26 +0200

    trying a new logo

--- client/src/pages/Landing/index.tsx
+++ client/src/pages/Landing/index.tsx
@@ -18,18 +18,14 @@ export default function LandingPage(): JSX.Element {
             <AboutApp />
             <Team />
             <Contact />
-            <footer>
-                <div className="content-container footer-container">
-                    <div className="footer-copyright">
-                        <img src={Logo} className="logo" alt="" width="70" height="24" />
-                        <p>
-                            &copy; <a href="index.html">ryoko</a>, 2021
-                        </p>
-                        <p>
-                            All rights reserved.
-                        </p>
-                    </div>
-                </div>
+            <footer className="content-container">
+                <img src={Logo} className="logo" alt="" width="70" height="24" />
+                <p>
+                    &copy; <a href="index.html">ryoko</a>, 2021
+                </p>
+                <p>
+                    All rights reserved.
+                </p>
             </footer>
         </Page>
     )
