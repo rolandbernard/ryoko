417ae7cfd39ad00fa8c0ca5b28ce4fde2440e5ca
Roland Bernard <rolbernard@unibz.it>
2021-06-24 23:24:31 +0200

    Improved the bar chart implementation

--- client/src/components/graphs/BarChart/index.tsx
+++ client/src/components/graphs/BarChart/index.tsx
@@ -1,27 +1,56 @@
 
-import { formatDate, addTime, currentTime, subtractTime } from 'timely';
+import { formatDate, addTime, currentTime } from 'timely';
 
 import { Activity } from 'adapters/common';
 
 import './bar-chart.scss';
 
 export interface ChartItem {
-    label: string;
+    label?: string;
     value: number;
 }
 
+/**
+ * Parse the activity data in the api format. The data will be formatted in a way where empty days
+ * will be interpolated with values of 0. This function will also create the labels based on the
+ * number of items in the result.
+ * 
+ * @param activity The list of activity item the api returns
+ * @param startDate The start date to start interpolating at
+ * @returns The ChartItems that can be used with the bar chart
+ */
 export function parseActivity(activity: Activity[], startDate?: Date): ChartItem[] {
     if (activity.length !== 0) {
-        const results = [];
+        let results: {
+            date: Date;
+            label?: string;
+            value: number;
+        }[] = [];
         let date = startDate ?? new Date(activity.map(act => act.day).reduce((a, b) => a < b ? a : b));
-        const isLong = date < subtractTime(currentTime(), 40, 'day');
         while (date <= currentTime()) {
             const day = date.toISOString().substr(0, 10);
             results.push({
-                label: isLong ? formatDate(date, 'month', undefined, false) : formatDate(date, 'none', 'short'),
+                date: date,
                 value: activity.find(item => item.day === day)?.time ?? 0,
             });
             date = addTime(date, 1, 'day');
+        }
+        for (let i = 0; i < results.length; i++) {
+            if (results.length < 10) {
+                results[i].label = formatDate(results[i].date, 'none', 'short');
+            } else if (results.length < 40) {
+                if (results[i].date.getDay() === 1) {
+                    results[i].label = formatDate(results[i].date, 'none', 'short');
+                }
+            } else if (results.length < 200) {
+                if (results[i].date.getDate() === 10) {
+                    results[i].label = formatDate(results[i].date, 'month', undefined, false);
+                }
+            } else if (results.length < 400) {
+                if (results[i].date.getDate() === 10 && results[i].date.getMonth() % 2 === 0) {
+                    results[i].label = formatDate(results[i].date, 'month', undefined, false);
+                }
+            }
         }
         return results;
     } else {
@@ -35,6 +64,12 @@ interface Props {
     multiplier?: number;
 }
 
+/**
+ * This component implements a bar chart. It will display all elements in the data property in the
+ * given order. The values in the multiplier and unit property will be used for displaying the value
+ * for each bar on hover in the tooltip. If an item includes a label, the label is displayed below
+ * the bar of the chart.
+ */
 export default function BarChart({ data, unit, multiplier }: Props) {
     let maxValue = data.map(e => e.value).reduce((a, b) => Math.max(a, b), 0);
 
@@ -53,16 +88,7 @@ export default function BarChart({ data, unit, multiplier }: Props) {
                                         width: Math.min((data.length < 40 ? 90 : 100) / data.length, 25) + '%'
                                     }}
                                 >
-                                    { (
-                                        data.length < 10
-                                            ? true
-                                            : (data.length < 40
-                                                ? (i % 7) === (data.length % 7 - 1)
-                                                : data.length < 120
-                                                    ? i !== 0 && item.label !== data[i - 1]?.label
-                                                    : (i % 80) === (data.length % 80 - 1)
-                                            )
-                                      ) &&
+                                    { item.label &&
                                         <div className="label">
                                             {item.label}
                                         </div>
